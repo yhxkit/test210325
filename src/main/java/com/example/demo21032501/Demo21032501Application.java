12ef4fd8974a049d985272f143ba911001ec6d76
yhxkit <yhxkit@gmail.com>
2021-03-25 22:51:08 +0900

    commit

---
+++ src/main/java/com/example/demo21032501/Demo21032501Application.java
@@ -0,0 +1,13 @@
+package com.example.demo21032501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo21032501Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo21032501Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo21032501/Demo21032501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo21032501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo21032501ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
